adb323c052903b62d779c9b627dfbe0ab07a94dd
TheRealPear <pearofficialyt@gmail.com>
2019-10-29 23:04:36 -0400

    Update CyberPatriot.bat

--- CyberPatriot.bat
+++ CyberPatriot.bat
@@ -1,8 +1,58 @@
 @ECHO OFF
 title CyberPatriot
+:admincheck
+NET SESSION >nul 2>&1
+IF %ERRORLEVEL% EQU 0 (
+    ECHO.
+    ECHO Administrator privileges detected!
+    GOTO loading
+) ELSE (
+    ECHO.
+    ECHO -----------------
+    ECHO   CyberPatriot
+    ECHO -----------------
+    ECHO.
+    ECHO Administrator privileges not found!
+    ECHO The script will not function correctly without administrator privileges.
+    ECHO.
+    ECHO Please re-open this script with administrator privileges.
+    ECHO You can do this by right-clicking on the script and click on "Run as administrator"
+    ECHO.
+    PAUSE
+    EXIT
+)
+
 :loading
 ECHO.
-ECHO -
-echo CyberPatriot Script
-ECHO -
-pause
+ECHO ---------------------------
+ECHO   Welcome to CyberPatriot
+ECHO ---------------------------
+ECHO.
+ECHO Before using this script, be sure you have done the following:
+ECHO.
+ECHO  [*] Follow the CyberPatriot rules.
+ECHO  [*] Use the correct image (Windows 8.1, 10, or Server 2016)
+ECHO  [*] The CCS service is running.
+ECHO  [*] Enter your team's unique number.
+ECHO  [*] Answer the forensic questions.
+ECHO.
+ECHO This script will cause irreversible changes to the image
+ECHO that may destroy evidence to help you answer the forensic questions.
+ECHO.
+PAUSE
+GOTO menu
+
+:menu
+CLS
+ECHO.
+ECHO -----------------
+ECHO   CyberPatriot
+ECHO -----------------
+ECHO.
+ECHO [A] Everything (automated)
+ECHO [B] Find Media Files
+ECHO [C] Fix Firewall
+ECHO [D] Windows Update
+ECHO [E] Disable Admin and Guest Account
+ECHO.
+PAUSE
