c30ef67ed9f6caaa31bfad57c26c6b31a97792b3
_Pear <pearofficialyt@gmail.com>
2019-11-13 07:39:56 -0500

    Update CyberPatriot.bat

--- Windows/CyberPatriot.bat
+++ Windows/CyberPatriot.bat
@@ -68,7 +68,7 @@ PAUSE
 GOTO menu
 
 :menu
-REM First interactive menu.
+REM Main interactive menu.
 CLS
 ECHO.
 ECHO -----------------
@@ -105,10 +105,14 @@ ECHO -----------------
 ECHO.
 ECHO Deleting prohibited media files...
 ECHO.
+REM Search the C: drive for all .mp2 files.
+DEL /s /q /f C:\*.mp2
 REM Search the C: drive for all .mp3 files.
 del /s /q /f C:\*.mp3
 ECHO.
 ECHO All prohibited media files has been deleted.
+PAUSE
+GOTO MENU
 
 :firewall
 CLS
@@ -117,8 +121,84 @@ ECHO -----------------
 ECHO   CyberPatriot
 ECHO -----------------
 ECHO.
-ECHO Attempting to repair Windows Firewall...
+ECHO Adding rules to the firewall...
+REM Disable all Remote Assistance ports.
+netsh advfirewall firewall set rule name="Remote Assistance (DCOM-In)" new enable=no >NUL
+netsh advfirewall firewall set rule name="Remote Assistance (PNRP-In)" new enable=no >NUL
+netsh advfirewall firewall set rule name="Remote Assistance (RA Server TCP-In)" new enable=no >NUL
+netsh advfirewall firewall set rule name="Remote Assistance (SSDP TCP-In)" new enable=no >NUL
+netsh advfirewall firewall set rule name="Remote Assistance (SSDP UDP-In)" new enable=no >NUL
+netsh advfirewall firewall set rule name="Remote Assistance (TCP-In)" new enable=no >NUL
+REM Disable telnet because it is an outdated application protocol.
+netsh advfirewall firewall set rule name="Telnet Server" new enable=no >NUL
+netsh advfirewall firewall set rule name="netcat" new enable=no >NUL
 ECHO.
+PAUSE
+GOTO MENU
+
+:weakservices
+CLS
+ECHO.
+ECHO -----------------
+ECHO   CyberPatriot
+ECHO -----------------
+ECHO.
+ECHO Disabling weak services...
+REM Disable IIS web server services.
+dism /online /disable-feature /featurename:IIS-WebServerRole >NUL
+dism /online /disable-feature /featurename:IIS-WebServer >NUL
+dism /online /disable-feature /featurename:IIS-CommonHttpFeatures >NUL
+dism /online /disable-feature /featurename:IIS-HttpErrors >NUL
+dism /online /disable-feature /featurename:IIS-HttpRedirect >NUL
+dism /online /disable-feature /featurename:IIS-ApplicationDevelopment >NUL
+dism /online /disable-feature /featurename:IIS-NetFxExtensibility >NUL
+dism /online /disable-feature /featurename:IIS-NetFxExtensibility45 >NUL
+dism /online /disable-feature /featurename:IIS-HealthAndDiagnostics >NUL
+dism /online /disable-feature /featurename:IIS-HttpLogging >NUL
+dism /online /disable-feature /featurename:IIS-LoggingLibraries >NUL
+dism /online /disable-feature /featurename:IIS-RequestMonitor >NUL
+dism /online /disable-feature /featurename:IIS-HttpTracing >NUL
+dism /online /disable-feature /featurename:IIS-Security >NUL
+dism /online /disable-feature /featurename:IIS-URLAuthorization >NUL
+dism /online /disable-feature /featurename:IIS-RequestFiltering >NUL
+dism /online /disable-feature /featurename:IIS-IPSecurity >NUL
+dism /online /disable-feature /featurename:IIS-Performance >NUL
+dism /online /disable-feature /featurename:IIS-HttpCompressionDynamic >NUL
+dism /online /disable-feature /featurename:IIS-WebServerManagementTools >NUL
+dism /online /disable-feature /featurename:IIS-ManagementScriptingTools >NUL
+dism /online /disable-feature /featurename:IIS-IIS6ManagementCompatibility >NUL
+dism /online /disable-feature /featurename:IIS-Metabase >NUL
+dism /online /disable-feature /featurename:IIS-HostableWebCore >NUL
+dism /online /disable-feature /featurename:IIS-StaticContent >NUL
+dism /online /disable-feature /featurename:IIS-DefaultDocument >NUL
+dism /online /disable-feature /featurename:IIS-DirectoryBrowsing >NUL
+dism /online /disable-feature /featurename:IIS-WebDAV >NUL
+dism /online /disable-feature /featurename:IIS-WebSockets >NUL
+dism /online /disable-feature /featurename:IIS-ApplicationInit >NUL
+dism /online /disable-feature /featurename:IIS-ASPNET >NUL
+dism /online /disable-feature /featurename:IIS-ASPNET45 >NUL
+dism /online /disable-feature /featurename:IIS-ASP >NUL
+dism /online /disable-feature /featurename:IIS-CGI >NUL
+dism /online /disable-feature /featurename:IIS-ISAPIExtensions >NUL
+dism /online /disable-feature /featurename:IIS-ISAPIFilter >NUL
+dism /online /disable-feature /featurename:IIS-ServerSideIncludes >NUL
+dism /online /disable-feature /featurename:IIS-CustomLogging >NUL
+dism /online /disable-feature /featurename:IIS-BasicAuthentication >NUL
+dism /online /disable-feature /featurename:IIS-HttpCompressionStatic >NUL
+dism /online /disable-feature /featurename:IIS-ManagementConsole >NUL
+dism /online /disable-feature /featurename:IIS-ManagementService >NUL
+dism /online /disable-feature /featurename:IIS-WMICompatibility >NUL
+dism /online /disable-feature /featurename:IIS-LegacyScripts >NUL
+dism /online /disable-feature /featurename:IIS-LegacySnapIn >NUL
+REM Disable FTP server services.
+dism /online /disable-feature /featurename:IIS-FTPServer >NUL
+dism /online /disable-feature /featurename:IIS-FTPSvc >NUL
+dism /online /disable-feature /featurename:IIS-FTPExtensibility >NUL
+dism /online /disable-feature /featurename:TFTP >NUL
+dism /online /disable-feature /featurename:TelnetClient >NUL
+dism /online /disable-feature /featurename:TelnetServer >NUL
+PAUSE
+GOTO menu
 
 :presemiautomation
 REM Confirm that the user wants to go with semi-automation.
