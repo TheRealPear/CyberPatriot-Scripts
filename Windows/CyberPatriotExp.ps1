4e0128fbca6a21cf079579f6ae7417f11647d91a
_Pear <pearofficialyt@gmail.com>
2019-12-01 12:34:26 -0500

    Update CyberPatriotExp.ps1

--- Windows/CyberPatriotExp.ps1
+++ Windows/CyberPatriotExp.ps1
@@ -1,3 +1,29 @@
-Write-Output "Currently in testing."
-Write-Output ""
-PAUSE
+function Show-Menu
+{
+    param (
+        [string]$Title = 'CyberPatriot'
+    )
+    Clear-Host
+    Write-Host "================ $Title ================"
+    Write-Host ""
+    Write-Host "A) Press '1' for this option."
+    Write-Host "B) Press '2' for this option."
+    Write-Host "C) Press '3' for this option."
+    Write-Host ""
+    
+}
+
+Show-Menu -Title "CyberPatriot"
+$selection = Read-Host "Please make a selection"
+switch ($selection)
+{
+    '1' {
+        'You chose option #1'
+    } '2' {
+        'You chose option #2'
+    } '3' {
+        'You chose option #3'
+    } 'q' {
+        return
+    }
+}
